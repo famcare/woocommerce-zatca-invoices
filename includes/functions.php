87fad41d9d83df6c94c32f487f1a70fba8a8aa4e
abdalsalaam <abdalsalaamnafez@gmail.com>
2022-03-06 19:15:13 +0300

    Add QR Code to PDF invoice - WCDN Plugin

--- includes/functions.php
+++ includes/functions.php
@@ -27,3 +27,21 @@ function famcare_generate_qr_code( $order_id ) {
 
 	(new Famcare_TLV_QR)->generateTLVImage($order_id,$order_date,$order_total,$order_tax);
 }
+
+/**
+ * Add QR Code to PDF invoice
+ * Print Invoice & Delivery Notes for WooCommerce Plugin
+ * https://wordpress.org/plugins/woocommerce-delivery-notes/
+ */
+function famcare_wcdn_after_branding( WC_Order $order ){
+	$_qr_code_image_id = get_post_meta($order->get_id(),'_qr_code_image_id',true);
+	$_qr_code_image_url = wp_get_attachment_url($_qr_code_image_id);
+	if($_qr_code_image_url && $_qr_code_image_id != ''){
+		?>
+		<div class="invoice-QR">
+			<img src="<?php echo $_qr_code_image_url;?>" alt="QR Code" width="100px">
+		</div>
+		<?php
+	}
+}
+add_action( 'wcdn_after_branding', 'famcare_wcdn_after_branding', 10, 1 );
